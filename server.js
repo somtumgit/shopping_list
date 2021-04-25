c63a5540709be3ed02fbf3a2fefa67e1ea251302
somtumgit <oajkk657@gmail.com>
2021-04-25 09:30:03 +0700

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;;
 
 
 //  Bodyparser Middleware
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.urlencoded());
 
